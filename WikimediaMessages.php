ec78f64fe035226620a5ecb751f6a7b67f387275
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-10 18:32:10 +0000

    * (bug 14165, bug 14294) Wikimedia specific configuration in convertGrammar() for 13 localisations (be, be-tarask, bs, cs, dsb, ga, gsw, hsb, hu, lv, rmy, sk, sl) removed. The settings have been put in extension WikimediaMessages. For be, be-tarask, bs, dsb, hsb some duplicates (1=1) were removed. Patch for Czech by Danny B. * Also put the wgGrammarForms from the Wikimedia configuration in here, so everything is in one place. Bonus with this setup is that there will be a little less work for Wikimedia shell operators, as the wgGrammarForms configuration is now accessible to i18n committers. * Header for WikimediaMessages.php, and add myself to extension credits.
    WARNING to Wikimedia shell operators: the whole section "wgGrammarForms" in InitialiseSettings.php.html should be remove after updating to this version. It will most probably overrule all settings in WikimediaMessages because of "'default' => array()"

--- WikimediaMessages.php
+++ WikimediaMessages.php
@@ -1,16 +1,28 @@
 <?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * An extension that adds Wikimedia specific functionality
+ *
+ * @addtogroup Extensions
+ *
+ * @copyright Copyright © 2008, Tim Starling
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
 
 $wgExtensionCredits['other'][] = array(
 	'name'           => 'WikimediaMessages',
-	'author'         => 'Tim Starling',
+	'author'         => array( 'Tim Starling', 'Siebrand Mazeland' ),
 	'svn-date'       => '$LastChangedDate$',
 	'svn-revision'   => '$LastChangedRevision$',
 	'description'    => 'Wikimedia specific messages',
 	'descriptionmsg' => 'wikimediamessages-desc',
 );
 
-$wgExtensionMessagesFiles['WikimediaMessages'] = dirname(__FILE__).'/WikimediaMessages.i18n.php';
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['WikimediaMessages'] = $dir .'WikimediaMessages.i18n.php';
 $wgExtensionFunctions[] = 'wfSetupWikimediaMessages';
+
+include_once ( $dir .'WikimediaGrammarForms.php' );
 
 function wfSetupWikimediaMessages() {
 	wfLoadExtensionMessages('WikimediaMessages');
