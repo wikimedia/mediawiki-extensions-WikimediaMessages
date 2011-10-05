8ec653caafa07c349bcc507286c679392ee3124b
Chad Horohoe <demon@users.mediawiki.org>
2011-10-05 02:05:14 +0000

    Add cant-delete-main-page hack msg to WikimediaMessages. Doesn't work in 1.18+ as-is

--- WikimediaMessages.i18n.php
+++ WikimediaMessages.i18n.php
@@ -158,6 +158,9 @@ You agree to be credited by re-users, at minimum, through a hyperlink or URL to 
 See the [http://wikimediafoundation.org/wiki/Terms_of_Use Terms of Use] for details.',
 	'wikimedia-editpage-tos-summary' => 'If you do not want your writing to be edited and redistributed at will, then do not submit it here.
 If you did not write this yourself, it must be available under terms consistent with the [http://wikimediafoundation.org/wiki/Terms_of_Use Terms of Use], and you agree to follow any relevant licensing requirements.',
+
+	# Custom message for enwiki to stop from deleting the mainpage
+	'cant-delete-main-page' => "You can't delete or move the main page.",
 );
 
 /** Message documentation (Message documentation)
