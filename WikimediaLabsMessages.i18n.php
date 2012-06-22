5ccd632a79e8c699404ffadba87bbea2d88e9722
Reedy <reedy@wikimedia.org>
2012-06-22 22:25:54 +0100

    Various Wikimedia Labs specific messages to be used on labsconsole for the GerritTools projects
    Change-Id: Ifed45228c582a4025bda37cf4c75462f122aefc7

---
+++ WikimediaLabsMessages.i18n.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Internationalisation file for extension Wikimedia Labs Messages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'wikimedialabsmessages-desc' => 'Wikimedia Labs messages',
+	'gerrittools-repogroup-analytics' => 'Wikimedia Analytics Gerrit repostories',
+	'gerrittools-repogroup-integration' => 'Wikimedia Continuous Integration Gerrit repostories',
+	'gerrittools-repogroup-mediawiki' => 'MediaWiki Gerrit repostories',
+	'gerrittools-repogroup-mediawiki/extensions' => 'MediaWik extension Gerrit repostories',
+	'gerrittools-repogroup-mediawiki/tools' => 'MediaWiki Tool Gerrit repostories',
+	'gerrittools-repogroup-operations' => 'Wikimedia Operations Gerrit repostories',
+	'gerrittools-repogroup-test' => 'Test Gerrit repostories',
+	'gerrittools-repogroup-wikimedia' => 'Wikimedia Gerrit repostories',
+);
+
+$messages['qqq'] = array(
+	'wikimedialabsmessages-desc' => '{{desc}}',
+	'gerrittools-repogroup-analytics' => 'Gerrit repostories for Wikimedia Analytic projects',
+	'gerrittools-repogroup-integration' => 'Gerrit repostories for Wikimedia Continuous Integration projects',
+	'gerrittools-repogroup-mediawiki' => 'Gerrit repostories for MediaWiki projects',
+	'gerrittools-repogroup-mediawiki/extensions' => 'Gerrit repostories for MediaWiki extensions',
+	'gerrittools-repogroup-mediawiki/tools' => 'Gerrit repostories for MediaWiki tools',
+	'gerrittools-repogroup-operations' => 'Gerrit repostories for Wikimedia Operations',
+	'gerrittools-repogroup-test' => 'Gerrit repostories for testing',
+	'gerrittools-repogroup-wikimedia' => 'Gerrit repostories for Wikimedia projects',
+);
