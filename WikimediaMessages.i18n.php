9dc54968a03a2258eacad592eee9bfb33849321b
raymond <raimond.spekking@gmail.com>
2013-02-22 12:14:31 +0100

    Use {{int:}} syntax to ensure consistent UI text
    Use protocol relative links
    
    UNTESTED patch. I am not sure that {{int:}} works here
    
    Change-Id: I2b5cdc5a803e15e3fa695056a9a03eea340d0695

--- WikimediaMessages.i18n.php
+++ WikimediaMessages.i18n.php
@@ -226,7 +226,7 @@ See <a href="//wikimediafoundation.org/wiki/Terms_of_Use">Terms of Use</a> for d
 text in the other namespaces is available under the <a href="//creativecommons.org/licenses/by-sa/3.0/" title="Definition of the Creative Commons Attribution/Share-Alike License">Creative Commons Attribution/Share-Alike License</a>;
 additional terms may apply.
 See <a href="//wikimediafoundation.org/wiki/Terms_of_Use" title="Wikimedia Foundation Terms of Use">Terms of Use</a> for details.',
-	'wikidata-shortcopyrightwarning' => 'By clicking "save", you agree to the [https://wikimediafoundation.org/wiki/Terms_of_Use terms of use], and you irrevocably agree to release your contribution under the [https://creativecommons.org/publicdomain/zero/1.0/ CC0 license].',
+	'wikidata-shortcopyrightwarning' => 'By clicking "{{int:wikibase-save}}", you agree to the [//wikimediafoundation.org/wiki/Terms_of_Use terms of use], and you irrevocably agree to release your contribution under the [//creativecommons.org/publicdomain/zero/1.0/ CC0 license].',
 	'wikimedia-copyrightwarning' => 'By clicking the "{{int:savearticle}}" button, you agree to the [//wikimediafoundation.org/wiki/Terms_of_Use Terms of Use], and you irrevocably agree to release your contribution under the [//en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License CC-BY-SA 3.0 License] and the [//en.wikipedia.org/wiki/Wikipedia:Text_of_the_GNU_Free_Documentation_License GFDL].
 You agree that a hyperlink or URL is sufficient attribution under the Creative Commons license.',
 
