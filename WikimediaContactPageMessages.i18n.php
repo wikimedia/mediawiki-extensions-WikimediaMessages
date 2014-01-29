6747749eff550169e8c9fc55cef6e18c55cdfde9
Reedy <reedy@wikimedia.org>
2014-01-29 22:15:16 +0000

    Add messages for Trademark Licensing form for foundationwiki
    Change-Id: I46f4a198b632622b56b009f3eb8965ea38178756

---
+++ WikimediaContactPageMessages.i18n.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Internationalisation file for ContactPage forms used by Wikimedia
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/**
+ * @author Reedy
+ */
+$messages['en'] = array(
+	'contactpage-title-requestlicense' => 'Trademark reuse contact form',
+	'contactpage-pagetext-requestlicense' => 'Please use the form below to contact us about Trademark things',
+	'contactpage-subject-requestlicense' => 'Wikimedia trademark reuse request',
+
+	'contactpage-license-request-username' => 'Username *',
+	'contactpage-license-request-relevantsite' => 'Relevant Wikimedia site (if any)',
+	'contactpage-license-request-group' => 'Wikimedia chapter, thematic organization, or user group & title (if any) *',
+	'contactpage-license-request-title' => 'Title (if applicable)',
+	'contactpage-license-request-organization' => 'Organization (if applicable)',
+	'contactpage-license-request-organization-type' => 'Organization type (if applicable)',
+	'contactpage-license-request-use-proposed' => 'Proposed use',
+	'contactpage-license-request-use-online' => 'Online (website, email)',
+	'contactpage-license-request-use-book' => 'Book, periodical',
+	'contactpage-license-request-use-print' => 'Print',
+	'contactpage-license-request-use-tv' => 'TV, movie or broadcast',
+	'contactpage-license-request-use-other' => 'Other',
+	'contactpage-license-request-use-note' => '<strong>Note:</strong> if we grant a trademark permission, we may list your use in a public list of authorized uses.',
+	'contactpage-license-request-description' => 'Description of usage',
+	'contactpage-license-request-selectmark' => 'Select mark',
+
+	'contactpage-wikimedia-trademark-globe' => 'Wikipedia Puzzle Globe',
+	'contactpage-wikimedia-trademark-wikiwordmark' => 'Wikipedia wordmark',
+	'contactpage-wikimedia-trademark-w' => 'Stylized "W"',
+	'contactpage-wikimedia-trademark-foundation' => 'Wikimedia Foundation',
+	'contactpage-wikimedia-trademark-commons' => 'Wikimedia Commons',
+	'contactpage-wikimedia-trademark-incubator' => 'Wikimedia Incubator',
+	'contactpage-wikimedia-trademark-mediawiki' => 'MediaWiki',
+	'contactpage-wikimedia-trademark-wikiquote' => 'Wikiquote',
+	'contactpage-wikimedia-trademark-wikibooks' => 'Wikibooks',
+	'contactpage-wikimedia-trademark-wikimania' => 'Wikimania',
+	'contactpage-wikimedia-trademark-wikimedia' => 'Wikimedia Community',
+	'contactpage-wikimedia-trademark-wikinews' => 'Wikinews',
+	'contactpage-wikimedia-trademark-wikisource' => 'Wikisource',
+	'contactpage-wikimedia-trademark-wikispecies' => 'Wikispecies',
+	'contactpage-wikimedia-trademark-wikiversity' => 'Wikiversity',
+	'contactpage-wikimedia-trademark-wiktionary' => 'Wiktionary',
+	'contactpage-wikimedia-trademark-wikivoyage' => 'Wikivoyage',
+
+	'contactpage-title-licenseabuse' => 'Copyright abuse contact form',
+	'contactpage-pagetext-licenseabuse' => 'Please use the form below to contact us about copyright abuse things',
+	'contactpage-subject-licenseabuse' => 'Wikimedia trademark abuse notification',
+
+	'contactpage-license-abuse-description' => 'Describe the abuse',
+	'contactpage-license-abuse-selectmark' => 'Which mark is abused?',
+	'contactpage-license-abuse-location' => 'Location of the abuse (on/offline)',
+	'contactpage-license-abuse-theircontact' => 'Contact information of person abusing the mark (if known)',
+	'contactpage-license-abuse-yourcontact' => 'Your contact information (if we may contact you)',
+);
+
+/**
+ * @author Reedy
+ */
+$messages['en-gb'] = array(
+	'contactpage-license-request-organization' => 'Organisation (if applicable)',
+	'contactpage-license-request-organization-type' => 'Organisation type (if applicable)',
+	'contactpage-license-request-use-note' => '<strong>Note:</strong> if we grant a trademark permission, we may list your use in a public list of authorised uses.',
+	'contactpage-wikimedia-trademark-w' => 'Stylised "W"',
+);
+
+/** Message documentation (Message documentation)
+ * @author Reedy
+ */
+$messages['qqq'] = array(
+	'contactpage-license-request-username' => 'The username of the person currently filling out this form',
+	'contactpage-license-request-relevantsite' => 'Which site a user is primarily active (Users home wiki)',
+	'contactpage-license-request-group' => 'Any affiliated chapters, groups etc relevant to this request',
+	'contactpage-license-request-title' => 'Salutation of a user',
+	'contactpage-license-request-organization' => 'Organization/company making this request to use Wikimedia trademarks',
+	'contactpage-license-request-organization-type' => 'Type of Organization/company making this request to use Wikimedia trademarks',
+	'contactpage-license-request-use-proposed' => 'Proposed usage of the Wikimedia trademarks',
+	'contactpage-license-request-use-online' => 'Request to use Wikimedia trademarks online on websites or emails',
+	'contactpage-license-request-use-book' => 'Request to use Wikimedia trademarks in books or periodicals',
+	'contactpage-license-request-use-print' => 'Request to use Wikimedia trademarks in print',
+	'contactpage-license-request-use-tv' => 'Request to use Wikimedia trademarks on tv, movie or other broadcast',
+	'contactpage-license-request-use-other' => 'Other potential usage locations of Wikimedia trademarks',
+	'contactpage-license-request-use-note' => 'Note advising that the Wikimedia Foundation may publicly list authorized usages of Wikimedia trademarks',
+	'contactpage-license-request-description' => 'Description of proposed usages of Wikimedia trademark(s)',
+	'contactpage-license-request-selectmark' => 'Select which Wikimedia tradmark(s)',
+
+	'contactpage-wikimedia-trademark-globe' => 'Wikipedia Globe logo',
+	'contactpage-wikimedia-trademark-wikiwordmark' => '',
+	'contactpage-wikimedia-trademark-w' => 'Stylized "W" (in Wikipedia logo font)',
+	'contactpage-wikimedia-trademark-foundation' => 'Wikimedia Foundation logo',
+	'contactpage-wikimedia-trademark-commons' => 'Wikimedia Commons logo',
+	'contactpage-wikimedia-trademark-incubator' => 'Wikimedia Incubator logo',
+	'contactpage-wikimedia-trademark-mediawiki' => 'MediaWiki logo',
+	'contactpage-wikimedia-trademark-wikiquote' => 'Wikiquote logo',
+	'contactpage-wikimedia-trademark-wikibooks' => 'Wikibooks logo',
+	'contactpage-wikimedia-trademark-wikimania' => 'Wikimania logo',
+	'contactpage-wikimedia-trademark-wikimedia' => 'Wikimedia community  logo',
+	'contactpage-wikimedia-trademark-wikinews' => 'Wikinews logo',
+	'contactpage-wikimedia-trademark-wikisource' => 'Wikisource logo',
+	'contactpage-wikimedia-trademark-wikispecies' => 'Wikispecies logo',
+	'contactpage-wikimedia-trademark-wikiversity' => 'Wikiversity logo',
+	'contactpage-wikimedia-trademark-wiktionary' => 'Wiktionary logo',
+	'contactpage-wikimedia-trademark-wikivoyage' => 'Wikivoyage logo',
+
+	'contactpage-license-abuse-description' => 'A description of the abuse of the use of Wikimedia trademarks',
+	'contactpage-license-abuse-selectmark' => 'Which Wikimedia trademark has been abused',
+	'contactpage-license-abuse-location' => 'Location of the abuse of Wikimedia trademark',
+	'contactpage-license-abuse-theircontact' => 'Contact details for the person abusing Wikimedia trademarks',
+	'contactpage-license-abuse-yourcontact' => 'Contact details of the person reporting the Wikimedia trademark abuse',
+);
