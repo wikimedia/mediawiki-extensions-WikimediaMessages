d2f99c7d4fe3bd5daa6810c3e078e97c00e422e6
Legoktm <legoktm.wikipedia@gmail.com>
2015-05-23 16:12:41 +0000

    Revert "Move hooks from closure to a hooks file"
    Hook WikimediaMessagesHooks::onMobileLicenseLink has invalid call signature; Parameter 3 to WikimediaMessagesHooks::onMobileLicenseLink() expected to be a reference, value given
    
    This reverts commit a584a6d7cd618218fd02d89978c6444e1b105672.
    
    Change-Id: Ica91d346d4a81e6b9733f37e13c9dfe2c07ff0ff

--- WikimediaMessages.php
+++ WikimediaMessages.php
@@ -15,10 +15,11 @@ $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'WikimediaMessages',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:WikimediaMessages',
-	'author'         => array( 'Tim Starling', 'Siebrand Mazeland', 'James D. Forrester' ),
+	'author'         => array( 'Tim Starling', 'Siebrand Mazeland' ),
 	'descriptionmsg' => 'wikimediamessages-desc',
 	'license-name'   => 'GPL-2.0+',
 );
+
 
 $wgMessagesDirs['WikimediaContactPageMessages'] = __DIR__ . '/i18n/contactpage';
 $wgMessagesDirs['WikimediaMessages'] = __DIR__ . '/i18n/wikimedia';
@@ -27,21 +28,176 @@ $wgMessagesDirs['WikimediaTemporaryMessages'] = __DIR__ . '/i18n/temporary';
 // Overrides of messages from core; must be consistent with MessageCache::get listener
 $wgMessagesDirs['WikimediaOverrideMessages'] = __DIR__ . '/i18n/wikimediaoverrides';
 
-$wgAutoloadClasses['WikimediaMessagesHooks'] = __DIR__ . '/WikimediaMessages.hooks.php';
-
 include_once ( __DIR__ .'/WikimediaGrammarForms.php' );
 
-// Register hooks
-$wgHooks['MessageCache::get'][] = 'WikimediaMessagesHooks::onMessageCacheGet';
-$wgHooks['SkinCopyrightFooter'][] = 'WikimediaMessagesHooks::onSkinCopyrightFooter';
-$wgHooks['EditPageCopyrightWarning'][] = 'WikimediaMessagesHooks::onEditPageCopyrightWarning';
-$wgHooks['MobileLicenseLink'][] = 'WikimediaMessagesHooks::onMobileLicenseLink';
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'WikimediaMessagesHooks::onSkinTemplateOutputPageBeforeExec';
-$wgHooks['TorBlockBlockedMsg'][] = 'WikimediaMessagesHooks::onTorBlockBlockedMsg';
-$wgHooks['GlobalBlockingBlockedIpMsg'][] = 'WikimediaMessagesHooks::onGlobalBlockingBlockedIpMsg';
-$wgHooks['GlobalBlockingBlockedIpXffMsg'][] = 'WikimediaMessagesHooks::onGlobalBlockingBlockedIpXffMsg';
-$wgHooks['MinervaPreRender'][] = 'WikimediaMessagesHooks::onMinervaPreRender';
-$wgHooks['UploadForm:initial'][] = 'WikimediaMessagesHooks::onUploadFormInitial';
+$wgExtensionFunctions[] = function() {
+	global $wgRightsUrl, $wgHooks;
+
+	if( strpos( $wgRightsUrl, 'creativecommons.org/licenses/by-sa/3.0' ) !== false ) {
+		// Override with Wikimedia's site-specific copyright message defaults
+		// with the CC/GFDL semi-dual license fun!
+
+		/**
+		 * @param $title Title
+		 * @param $type string
+		 * @param $msg string
+		 * @param $link
+		 * @return bool
+		 */
+		$wgHooks['SkinCopyrightFooter'][] = function( $title, $type, &$msg, &$link ) {
+			if ( $type !== 'history' ) {
+				global $wgDBname;
+				if ( in_array( $wgDBname, array( 'wikidatawiki', 'testwikidatawiki' ) ) ) {
+					$msg = 'wikidata-copyright';
+				} else {
+					$msg = 'wikimedia-copyright'; // the default;
+				}
+			}
+
+			return true;
+		};
+
+		$wgHooks['EditPageCopyrightWarning'][] = function( $title, &$msg ) {
+			$msg = array( 'wikimedia-copyrightwarning' );
+			return true;
+		};
+
+		$wgHooks['MobileLicenseLink'][] = function( &$link, $context, $attribs ) {
+			// We only display the dual licensing stack in the editor and talk interfaces
+			if ( $context === 'editor' || $context === 'talk' ) {
+				$link = wfMessage( 'wikimedia-mobile-license-links' )->plain();
+			}
+			return true;
+		};
+	}
+
+	//Bug 42231: Should have a specific message for WMF projects
+	$wgHooks['TorBlockBlockedMsg'][] = function( &$msg ) {
+		$msg = 'wikimedia-torblock-blocked';
+		return true;
+	};
+	$wgHooks['GlobalBlockingBlockedIpMsg'][] = function( &$msg ) {
+		$msg = 'wikimedia-globalblocking-ipblocked';
+		return true;
+	};
+	$wgHooks['GlobalBlockingBlockedIpXffMsg'][] = function( &$msg ) {
+		$msg = 'wikimedia-globalblocking-ipblocked-xff';
+		return true;
+	};
+
+};
+
+/**
+ * Bug 33464: Add a "Developers" link to the footer
+ *
+ * @param $skin SkinTemplate (from includes/SkinTemplate.php)
+ * @param $template QuickTemplate (from includes/SkinTemplate.php)
+ * @return bool
+ */
+$wgHooks['SkinTemplateOutputPageBeforeExec'][] = function( &$skin, &$template ) {
+	$destination = Skin::makeInternalOrExternalUrl( $skin->msg( 'wikimedia-developers-url' )->inContentLanguage()->text() );
+	$link = Html::element(
+		'a',
+		array( 'href' => $destination ),
+		$skin->msg( 'wikimedia-developers' )->text()
+	);
+	$template->set( 'developers', $link );
+	$template->data['footerlinks']['places'][] = 'developers';
+	return true;
+};
+
+$wgHooks['MessageCache::get'][] = 'wfWikimediaMessagesMessageCacheGet';
+
+/**
+ * When core requests certain messages, change the key to a Wikimedia version.
+ *
+ * @note Don't make this a closure, it causes the Database Dumps to fail.
+ *   See https://bugs.php.net/bug.php?id=52144
+ *
+ *   mwscript getSlaveServer.php --wiki='dewiki' --group=dump --globals
+ *   print_r( $GLOBALS['wgHooks']['MessageCache::get'] );
+ *
+ * @param String &$lcKey message key to check and possibly convert
+ * @return bool
+ */
+function wfWikimediaMessagesMessageCacheGet( &$lcKey ) {
+	global $wgLanguageCode;
+
+	static $keys = array(
+		'createacct-helpusername',
+		'createacct-imgcaptcha-help',
+		'feedback-termsofuse',
+		'globalrenamequeue-email-body-approved',
+		'globalrenamequeue-email-body-rejected',
+		'sidebar',
+		'sulrenamewarning-usenew',
+		'centralauth-contribs-locked',
+		'centralauth-uwbr-intro',
+		'centralauth-uwbr-registration-nodate',
+		'privacypage',
+	);
+
+	if ( in_array( $lcKey, $keys, true ) ) {
+		$prefixedKey ="wikimedia-$lcKey";
+
+		// MessageCache uses ucfirst if ord( key ) is < 128, which is true of all
+		// of the above.  Revisit if non-ASCII keys are used.
+		$ucKey = ucfirst( $lcKey );
+
+		$cache = MessageCache::singleton();
+		if (
+			// Override order:
+			// 1. If the MediaWiki:$ucKey page exists, use the key unprefixed
+			// (in all languages) with normal fallback order.  Specific
+			// language pages (MediaWiki:$ucKey/xy) are not checked when
+			// deciding which key to use, but are still used if applicable
+			// after the key is decided.
+			//
+			// 2. Otherwise, use the prefixed key with normal fallback order
+			// (including MediaWiki pages if they exist).
+			$cache->getMsgFromNamespace( $ucKey, $wgLanguageCode ) === false
+		) {
+			$lcKey = $prefixedKey;
+		}
+	}
+	return true;
+};
+
+/**
+ * Add a WMF-specific footer link to terms of use on mobile site
+ * Overrides template data right before it gets sent to template for rendering
+ * @param MinervaTemplate $tpl
+ *
+ * @return bool
+ */
+$wgHooks['MinervaPreRender'][] = function( $tpl ) {
+	$skin = $tpl->getSkin();
+	// This will work only on mobile site because only SkinMobile has this method
+	if ( method_exists( $skin, 'getTermsLink' ) ) {
+		$tpl->set( 'terms-use', $skin->getTermsLink( 'wikimedia-mobile-terms-url' ) );
+	}
+	return true;
+};
+
+/**
+ * Require the creation of MediaWiki:Licenses to enable uploading.
+ * This should prevent a lot of uploading without licenses on small wikis;
+ * some or many of the small WMF wikis do not have any license options,
+ * which is really needed for our copyright policy.
+ *
+ * Do not require it when licenses is in $wgForceUIMsgAsContentMsg,
+ * to prevent checking each subpage of MediaWiki:Licenses.
+ */
+$wgHooks['UploadForm:initial'][] = function() {
+	global $wgForceUIMsgAsContentMsg;
+	if ( !in_array( 'licenses', $wgForceUIMsgAsContentMsg )
+		&& wfMessage( 'licenses' )->inContentLanguage()->isDisabled()
+	) {
+		throw new ErrorPageError( 'uploaddisabled', 'wikimedia-upload-nolicenses' );
+	}
+	return true;
+};
+
 
 $wgResourceModules['ext.wikimediamessages.contactpage.affcomusergroup'] = array(
 	'localBasePath' => __DIR__ . '/modules',
