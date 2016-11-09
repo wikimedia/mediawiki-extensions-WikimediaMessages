491657325056046dc7dd9acf01e8d8d00e949fb6
Antoine Musso <hashar@free.fr>
2016-11-09 15:37:11 +0100

    Add npm entry point and banana i18n linter
    Bug: T150332
    Change-Id: I288e902ec922cff36f2dbd3332644bbf527e6301

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		banana: {
+			all: [
+				'i18n/*/',
+			]
+		}
+	} );
+
+	grunt.registerTask( 'lint', ['banana'] );
+	grunt.registerTask( 'test', ['lint'] );
+	grunt.registerTask( 'default', ['test'] );
+};
