996d7a8c529b165c606906d09724525f028a0463
Stephane Bisson <sbisson@wikimedia.org>
2017-03-22 09:35:25 -0400

    RC filters: highlight guided tour
    Soft dependency on I16009c5d96f2cafddb8d545eedbf9ca1e11d06c6
    
    Bug: T151006
    Change-Id: Idf8196894cc5d7307934e615640074a108598829

---
+++ modules/rcfilters-highlight-tour.js
@@ -0,0 +1,35 @@
+/*!
+ * Guided tour for highlight feature
+ */
+( function ( mw, gt ) {
+	var tour = new gt.TourBuilder( {
+			name: 'RcFiltersHighlight',
+			shouldLog: true,
+			isSinglePage: true
+		} );
+
+	tour.firstStep( {
+		name: 'Highlight',
+		titlemsg: 'eri-rcfilters-tour-highlight-title',
+		description: mw.message( 'eri-rcfilters-tour-highlight-description' ).text(),
+		autoFocus: true,
+		allowAutomaticOkay: false,
+		attachTo: '.mw-rcfilters-ui-filtersListWidget-hightlightButton',
+		position: 'right',
+		buttons: [
+			{
+				namemsg: 'eri-rcfilters-tour-highlight-button',
+				onclick: function () {
+					gt.endTour();
+				}
+			}
+		],
+		onShow: function () {
+			// Put the guider inside the overlay to prevent
+			// it's close button from closing the rc filters popup
+			var $overlay = $( '.mw-rcfilters-ui-overlay' ),
+				$guider = $( '.mw-guidedtour-tour-RcFiltersHighlight' );
+			$overlay.append( $guider );
+		}
+	} );
+}( mediaWiki, mediaWiki.guidedTour ) );
