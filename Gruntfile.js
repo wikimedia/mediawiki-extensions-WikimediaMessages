51bb8d49ef900de414ebc7581a8adcb420f95bd5
Umherirrender <umherirrender_de.wp@web.de>
2017-04-18 10:13:08 +0200

    Add stylelint for css and less files
    Fixed the following rules:
    indentation
    number-leading-zero
    selector-pseudo-class-parentheses-space-inside
    
    Change-Id: I1745bb1035c76b63d43f19a55dab673abe9e7f86

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: {
@@ -11,13 +12,22 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
+				'.stylelintrc',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'**/*.less',
 				'!node_modules/**',
 				'!vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'lint', ['jsonlint', 'banana'] );
+	grunt.registerTask( 'lint', ['jsonlint', 'banana', 'stylelint'] );
 	grunt.registerTask( 'test', ['lint'] );
 	grunt.registerTask( 'default', ['test'] );
 };
