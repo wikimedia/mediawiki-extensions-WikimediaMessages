ef229d9b9e8f5c5613af6c57d375a9a77ea93b23
Taavi Väänänen <hi@taavi.wtf>
2022-01-07 15:37:47 +0200

    Add placeholders for UserMerge log entries
    This is following the strategy described in T222918, that is creating
    copies of the UserMerge messages and storing them with a Wikimedia
    prefix and asking Translatewiki to do a one-off copy from the original
    messages to the new ones.
    
    Bug: T216089
    Bug: T218160
    Bug: T222918
    Change-Id: Ic138bd40e289df155337e21b66314ccf66048213

---
+++ includes/LogFormatter/WMUserMergeLogFormatter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MediaWiki\Extension\WikimediaMessages\LogFormatter;
+
+use LogFormatter;
+
+/**
+ * A snapshot of the log formatter from the UserMerge
+ * extension, git revision e42022c82c8a0224aca9a2fab884259133f48df4.
+ *
+ * @see https://phabricator.wikimedia.org/T218160
+ */
+class WMUserMergeLogFormatter extends LogFormatter {
+
+	/** @var int[] */
+	private static $mapLegacy = [
+		'oldName' => 0,
+		'oldId' => 1,
+		'newName' => 2,
+		'newId' => 3,
+	];
+
+	/**
+	 * @return string
+	 */
+	protected function getMessageKey() {
+		if ( $this->entry->getSubtype() === 'deleteuser' ) {
+			return 'wikimedia-logentry-usermerge-deleteuser';
+		} else {
+			return 'wikimedia-logentry-usermerge-mergeuser';
+		}
+	}
+
+	/**
+	 * @param string $param
+	 * @return mixed
+	 */
+	private function getParameter( $param ) {
+		$parameters = $this->entry->getParameters();
+		if ( $this->entry->isLegacy() ) {
+			$param = self::$mapLegacy[$param];
+		}
+		return $parameters[$param];
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function extractParameters() {
+		$params = [];
+		// 0-2 are set in LogFormatter::getMessageParameters()
+		$params[3] = $this->getParameter( 'oldName' );
+		$params[4] = $this->getParameter( 'oldId' );
+		if ( $this->entry->getSubtype() !== 'deleteuser' ) {
+			$params[5] = $this->formatParameterValue(
+				'user-link', $this->getParameter( 'newName' ) );
+			$params[6] = $this->getParameter( 'newId' );
+			$params[7] = $this->formatParameterValue(
+				'user', $this->getParameter( 'newName' ) );
+		}
+		return $params;
+	}
+
+}
