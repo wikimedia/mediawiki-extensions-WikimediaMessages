63c1fa6847e36521035d94797b895a43da0358e7
Steven Sun <seedless_steven@outlook.com>
2022-04-12 21:34:32 +0800

    IP Info: Align feedback button with text
    Bug: T305815
    Change-Id: I8e9f748427730f89744849fc365268808e6031b6

--- modules/ext.wikimediaMessages.ipInfo.hooks/feedback.js
+++ modules/ext.wikimediaMessages.ipInfo.hooks/feedback.js
@@ -7,7 +7,7 @@
 			href: 'https://meta.wikimedia.org/wiki/IP_Editing:_Privacy_Enhancement_and_Abuse_Mitigation/IP_Info_feature/Feedback?action=edit&section=new&preload=Special:MyLanguage/Template:IPInfoFeedback',
 			label: mw.msg( 'ipinfo-feedback-button-label' ),
 			icon: 'feedback',
-			classes: [ 'ext-ipinfo-feedback-button' ]
+			classes: [ 'ext-ipinfo-feedback-button', 'ext-ipinfo-widget-property' ]
 		} );
 		$info.find( '.ext-ipinfo-widget-property-source' ).before( feedbackButton.$element );
 	} );
