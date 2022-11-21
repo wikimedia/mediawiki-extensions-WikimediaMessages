cbaafb247b13521bc9b7927d433e0ef1da26d093
Daimona Eaytoy <daimona.wiki@gmail.com>
2022-11-21 19:10:51 +0100

    Add another policy message for the CampaignEvents extension
    Bug: T318375
    Depends-On: Ia238dc2e3885b56ac4d5365f67adb4696e106b85
    Change-Id: Idfaf113e6aa37e6d784e3c1c6d63b93e29a7827b

--- includes/WikimediaMessagesCampaignEventsHooks.php
+++ includes/WikimediaMessagesCampaignEventsHooks.php
@@ -2,14 +2,25 @@
 
 namespace MediaWiki\Extension\WikimediaMessages;
 
+use MediaWiki\Extension\CampaignEvents\Hooks\CampaignEventsGetPolicyMessageForRegistrationFormHook;
 use MediaWiki\Extension\CampaignEvents\Hooks\CampaignEventsGetPolicyMessageForRegistrationHook;
 
-class WikimediaMessagesCampaignEventsHooks implements CampaignEventsGetPolicyMessageForRegistrationHook {
+class WikimediaMessagesCampaignEventsHooks implements
+	CampaignEventsGetPolicyMessageForRegistrationHook,
+	CampaignEventsGetPolicyMessageForRegistrationFormHook
+{
 
 	/**
 	 * @inheritDoc
 	 */
 	public function onCampaignEventsGetPolicyMessageForRegistration( ?string &$message ) {
 		$message = 'ext-campaignevents-register-policy';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onCampaignEventsGetPolicyMessageForRegistrationForm( ?string &$message ) {
+		$message = 'ext-campaignevents-registration-form-policy';
 	}
 }
