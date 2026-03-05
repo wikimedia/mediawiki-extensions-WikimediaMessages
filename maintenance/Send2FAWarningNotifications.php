d8790597e7cc101337a147045804a0fb43694ba7
Marcin Szwarc <mszwarc@wikimedia.org>
2026-03-05 14:10:43 +0100

    Add a script to send mandatory 2FA Echo notification
    Why:
    - Before we start demoting 2FA-less users from 2FA-required groups,
      we'd like to warn the users. Warnings are sent by e-mails and
      on-wiki notifications (Echo).
    
    What:
    - Add a maintenance script to send notifications that they will lose
      their groups unless they configure 2FA.
    
    Bug: T419111
    Change-Id: Ia33192757fca2e880171a420d17edffb389e87bf
    (cherry picked from commit 001e917d77cb433e63aa71e8429d8dbce0bbc08c)

---
+++ maintenance/Send2FAWarningNotifications.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MediaWiki\Extension\WikimediaMessages\Maintenance;
+
+use MediaWiki\Maintenance\Maintenance;
+use MediaWiki\Notification\Notification;
+use MediaWiki\Notification\RecipientSet;
+use MediaWiki\WikiMap\WikiMap;
+
+class Send2FAWarningNotifications extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'WikimediaMessages' );
+		$this->addDescription( 'Send notifications about mandatory 2FA to users (read from stdin)' );
+		$this->addOption( 'deadline', 'Deadline for 2FA compliance in YYYYMMDD format', true, true );
+		$this->setBatchSize( 100 );
+	}
+
+	public function execute() {
+		$services = $this->getServiceContainer();
+		$userIdentityLookup = $services->getUserIdentityLookup();
+		$notifications = $services->getNotificationService();
+
+		$deadline = $this->getOption( 'deadline' );
+		if ( !preg_match( '/^\d{8}$/', $deadline ) ) {
+			$this->error( "Invalid deadline format, expected YYYYMMDD: $deadline" );
+			return;
+		}
+		// Notifications expect a timestamp, so append time to the date and convert to timestamp
+		$deadline .= '000000';
+
+		$text = $this->getStdin( Maintenance::STDIN_ALL );
+		$success = 0;
+		foreach ( explode( "\n", $text ) as $i => $userName ) {
+			if ( trim( $userName ) === '' ) {
+				continue;
+			}
+			if ( str_contains( $userName, '@' ) ) {
+				[ $userName, $wikiId ] = explode( '@', $userName, 2 );
+				$userName = trim( $userName );
+				$wikiId = trim( $wikiId );
+				if ( !WikiMap::isCurrentWikiId( $wikiId ) ) {
+					continue;
+				}
+			}
+
+			$userIdentity = $userIdentityLookup->getUserIdentityByName( $userName );
+			if ( !$userIdentity ) {
+				$this->error( ( $i + 1 ) . ": User is invalid or not registered: $userName" );
+				continue;
+			}
+			$notifications->notify(
+				new Notification( '2fa-warning', [ 'deadline' => $deadline ] ),
+				new RecipientSet( $userIdentity )
+			);
+			$this->output( ( $i + 1 ) . ": $userName\n" );
+			$success++;
+			if ( $success % $this->getBatchSize() === 0 ) {
+				$this->waitForReplication();
+			}
+		}
+
+		$this->output( "Notified $success users.\n" );
+	}
+}
+
+$maintClass = Send2FAWarningNotifications::class;
